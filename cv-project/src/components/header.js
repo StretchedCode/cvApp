9d0f0e72120eefec976deedc114f6d59c9c8d726
Ghufran <ghufranshahid101@gmail.com>
2023-06-18 16:49:41 -0400

    Added header component

---
+++ cv-project/src/components/header.js
@@ -0,0 +1,19 @@
+import React, { Component }from "react";
+
+class Header extends Component{
+
+    constructor(){
+        super()
+    }
+
+    render(){
+
+        return (
+            <div className="header">
+                CV Application
+            </div>
+        )
+    }
+}
+
+export default Header
